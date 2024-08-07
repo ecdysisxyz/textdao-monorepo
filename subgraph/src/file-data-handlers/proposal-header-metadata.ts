71a457ef3c6f9a64eb02686849c1420416c546a3
Kai Hiroi <kai.hiroi@pm.me>
2024-08-07 15:02:24 +0900

    feat(subgraph): enable logging at adding metadata field

--- subgraph/src/file-data-handlers/proposal-header-metadata.ts
+++ subgraph/src/file-data-handlers/proposal-header-metadata.ts
@@ -2,31 +2,27 @@ import { ipfs, json, log } from "@graphprotocol/graph-ts";
 import { Header } from "../../generated/schema";
 
 export function saveHeaderMetadata(metadataCid: string, header: Header): void {
-    if (!metadataCid) return;
-    const metadata = ipfs.cat(metadataCid);
-    if (!metadata) return;
-    const obj = json.fromBytes(metadata).toObject();
-    if (obj) {
-        const title = obj.get("title");
-        const body = obj.get("body");
+	if (!metadataCid) return;
+	const metadata = ipfs.cat(metadataCid);
+	if (!metadata) return;
+	const obj = json.fromBytes(metadata).toObject();
+	if (obj) {
+		const title = obj.get("title");
+		const body = obj.get("body");
 
-        if (title) {
-            header.title = title.toString();
-        } else {
-            log.info(
-                "`title` field not found in the proposal header content",
-                []
-            );
-        }
-        if (body) {
-            header.body = body.toString();
-        } else {
-            log.info(
-                "`body` field not found in the proposal header content",
-                []
-            );
-        }
+		if (title) {
+			header.title = title.toString();
+			log.info("`title` added: ", [title.toString()]);
+		} else {
+			log.info("`title` field not found in the proposal header content", []);
+		}
+		if (body) {
+			header.body = body.toString();
+			log.info("`body` added: ", [body.toString()]);
+		} else {
+			log.info("`body` field not found in the proposal header content", []);
+		}
 
-        header.save();
-    }
+		header.save();
+	}
 }
