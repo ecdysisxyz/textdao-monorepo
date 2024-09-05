0fd30152bbf84962cc66e02735713b0167903a5e
Kai Hiroi <kai.hiroi@pm.me>
2024-09-05 23:57:36 +0900

    docs(setting): allow broken link

--- packages/documentation/docusaurus.config.ts
+++ packages/documentation/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   organizationName: "ecdysisxyz",
   projectName: "textdao-monorepo",
 
-  onBrokenLinks: "throw",
+  onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
